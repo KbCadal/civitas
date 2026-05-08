7a7453c7e35b88e68a5bd165267d6390ab743e8f
Wisnu Nugroho <wisnunugroho614@gmail.com>
2026-05-08 21:56:51 +0700

    chore: remove .TODELETE

--- .TODELETE/server/api/dstipro/datatable/index.get.ts
+++
@@ -1,7 +0,0 @@
-import { db } from '@/server/fake-db/dstipro/datatable'
-
-export default defineEventHandler(async event => {
-  setResponseStatus(event, 200)
-
-  return db.salesDetails
-})
--- .TODELETE/server/api/dstipro/profile/header.get.ts
+++
@@ -1,7 +0,0 @@
-import { db } from '@/server/fake-db/dstipro/profile'
-
-export default defineEventHandler(async event => {
-  setResponseStatus(event, 200)
-
-  return db.data.profileHeader
-})
